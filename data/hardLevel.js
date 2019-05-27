ef4b2d5e7dee9f2483c5a8e6e0c71b09a099d499
paguioc <31397592+paguioc@users.noreply.github.com>
2019-05-27 19:58:52 +1000

    Add files via upload

---
+++ data/hardLevel.js
@@ -0,0 +1,267 @@
+/*
+  This contains the data for the hard level.
+*/
+
+/* Question and answer game data */
+window.hardLevel = [
+  {
+    question: "10 * 10 = ",
+    answer: 100
+  },
+  {
+    question: "100 + 5 = ",
+    answer: 105
+  },
+  {
+    question: "34 + 10 = ",
+    answer: 44
+  },
+  {
+    question: "16 + 24 = ",
+    answer: 40
+  },
+  {
+    question: "73 - 19 = ",
+    answer: 54
+  },
+  {
+    question: "55 - 16 = ",
+    answer: 39
+  },
+  {
+    question: "17 - 22 = ",
+    answer: -5
+  },
+  {
+    question: "18 * 3 = ",
+    answer: 54
+  },
+  {
+    question: "96 / 12 = ",
+    answer: 8
+  },
+  {
+    question: "125 / 25 = ",
+    answer: 5
+  }
+];
+
+/* Memory game data */
+window.memoryGameHardElements = [
+  {
+    name: "one",
+    image: "images/one.png"
+  },
+  {
+    name: "two",
+    image: "images/two.png"
+  },
+  {
+    name: "three",
+    image: "images/three.png"
+  },
+  {
+    name: "four",
+    image: "images/four.png"
+  },
+  {
+    name: "five",
+    image: "images/five.png"
+  },
+  {
+    name: "six",
+    image: "images/six.png"
+  },
+  {
+    name: "seven",
+    image: "images/seven.png"
+  },
+  {
+    name: "eight",
+    image: "images/eight.png"
+  },
+  {
+    name: "nine",
+    image: "images/nine.png"
+  }
+
+];
+
+window.memoryGameHardElements2 = [
+  {
+    name: "one",
+    image: "images/raw/one.png"
+  },
+  {
+    name: "two",
+    image: "images/raw/two.png"
+  },
+  {
+    name: "three",
+    image: "images/raw/three.png"
+  },
+  {
+    name: "four",
+    image: "images/raw/four.png"
+  },
+  {
+    name: "five",
+    image: "images/raw/five.png"
+  },
+  {
+    name: "six",
+    image: "images/raw/six.png"
+  },
+  {
+    name: "seven",
+    image: "images/raw/seven.png"
+  },
+  {
+    name: "eight",
+    image: "images/raw/eight.png"
+  },
+  {
+    name: "nine",
+    image: "images/raw/nine.png"
+  }
+
+];
+
+/* Multiple Choice game data */
+window.multipleChoiceHardElements = [
+  {
+    questionImage: "images/raw/mc1.jpg",
+    options: {
+      a: "98",
+      b: "81",
+      c: "91"
+    },
+    answer: "b"
+  },
+  {
+    questionImage: "images/raw/mc3.jpg",
+    options: {
+      a: "7",
+      b: "8",
+      c: "9"
+    },
+    answer: "c"
+  },
+  {
+    questionImage: "images/raw/mc4.jpg",
+    options: {
+      a: "68",
+      b: "71",
+      c: "72"
+    },
+    answer: "c"
+  },
+  {
+    questionImage: "images/raw/mc5.jpg",
+    options: {
+      a: "27",
+      b: "26",
+      c: "24"
+    },
+    answer: "b"
+  },
+  {
+    questionImage: "images/raw/mc2.jpg",
+    options: {
+      a: "-26",
+      b: "74",
+      c: "26"
+    },
+    answer: "a"
+  },
+  {
+    questionImage: "images/raw/mc6.jpg",
+    options: {
+      a: "70",
+      b: "48",
+      c: "51"
+    },
+    answer: "b"
+  },
+  {
+    questionImage: "images/raw/mc7.jpg",
+    options: {
+      a: "10",
+      b: "18",
+      c: "11"
+    },
+    answer: "a"
+  },
+  {
+    questionImage: "images/raw/mc8.jpg",
+    options: {
+      a: "56",
+      b: "48",
+      c: "51"
+    },
+    answer: "a"
+  },
+  {
+    questionImage: "images/raw/mc9.jpg",
+    options: {
+      a: "70",
+      b: "60",
+      c: "51"
+    },
+    answer: "b"
+  },
+  {
+    questionImage: "images/raw/mc10.jpg",
+    options: {
+      a: "60",
+      b: "58",
+      c: "57"
+    },
+    answer: "c"
+  }
+];
+
+/* Matching type game data */
+window.matchingGameHardProblems = [
+
+    "74 - 56",
+    "12 + 13",
+    "18 + 21"
+];
+
+window.matchingGameHardElements = [
+
+    "eighteen",
+    "twentyFive",
+    "thirtyNine"
+];
+
+
+window.matchingGameHardProblems2 = [
+
+    "46 / 2",
+    "9 * 11",
+    "110 - 86"
+];
+
+window.matchingGameHardElements2 = [
+
+    "thirteen",
+    "ninetyNine",
+    "twentyFour"
+];
+
+
+window.matchingGameHardProblems3 = [
+
+    "65 - 24",
+    "13 * 3",
+    "44 + 38"
+];
+
+window.matchingGameHardElements3 = [
+
+    "fortyOne",
+    "thirtyNine",
+    "eightyTwo"
+];
---
+++ data/mediumLevel.js
@@ -0,0 +1,235 @@
+/*
+  This contains the data for the medium level.
+*/
+
+/* Question and answer data */
+window.mediumLevel = [
+  {
+    question: "2 + 10 = ",
+    answer: 12
+  },
+  {
+    question: "5 + 8 = ",
+    answer: 13
+  },
+  {
+    question: "16 - 4 = ",
+    answer: 12
+  },
+  {
+    question: "9 - 6 = ",
+    answer: 3
+  },
+  {
+    question: "6 * 3 = ",
+    answer: 18
+  },
+  {
+    question: "2 * 5 = ",
+    answer: 10
+  },
+  {
+    question: "12 / 2 = ",
+    answer: 6
+  },
+  {
+    question: "15 / 5 = ",
+    answer: 3
+  },
+  {
+    question: "20 / 2 = ",
+    answer: 10
+  },
+  {
+    question: "18 / 6 = ",
+    answer: 3
+  }
+];
+
+/* Memory game data */
+window.memoryGameMediumElements = [
+  {
+    name: "one",
+    image: "images/raw/one.jpg"
+  },
+  {
+    name: "two",
+    image: "images/raw/two.jpg"
+  },
+  {
+    name: "three",
+    image: "images/raw/three.jpg"
+  },
+  {
+    name: "four",
+    image: "images/raw/four.jpg"
+  },
+  {
+    name: "five",
+    image: "images/raw/five.jpg"
+  },
+  {
+    name: "six",
+    image: "images/raw/six.jpg"
+  },
+  {
+    name: "seven",
+    image: "images/raw/seven.jpg"
+  },
+  {
+    name: "eight",
+    image: "images/raw/eight.jpg"
+  },
+  {
+    name: "nine",
+    image: "images/raw/nine.jpg"
+  }
+
+];
+
+/* Multiple Choice game data */
+window.multipleChoiceMediumElements = [
+  {
+    number: 9,
+    animalImage: "images/donkey.png",
+    options: {
+      a: "8",
+      b: "10",
+      c: "9"
+    },
+    answer: "c"
+  },
+  {
+    number: 7,
+    animalImage: "images/pig.png",
+    options: {
+      a: "7",
+      b: "8",
+      c: "9"
+    },
+    answer: "a"
+  },
+  {
+    number: 10,
+    animalImage: "images/sheep.png",
+    options: {
+      a: "10",
+      b: "9",
+      c: "11"
+    },
+    answer: "a"
+  },
+  {
+    number: 6,
+    animalImage: "images/chicken2.png",
+    options: {
+      a: "6",
+      b: "9",
+      c: "5"
+    },
+    answer: "a"
+  },
+  {
+    number: 11,
+    animalImage: "images/egg.png",
+    options: {
+      a: "10",
+      b: "11",
+      c: "7"
+    },
+    answer: "b"
+  },
+  {
+    number: 8,
+    animalImage: "images/goat2.png",
+    options: {
+      a: "7",
+      b: "8",
+      c: "5"
+    },
+    answer: "b"
+  },
+  {
+    number: 14,
+    animalImage: "images/duck.png",
+    options: {
+      a: "14",
+      b: "13",
+      c: "15"
+    },
+    answer: "a"
+  },
+  {
+    number: 12,
+    animalImage: "images/chick.png",
+    options: {
+      a: "16",
+      b: "13",
+      c: "12"
+    },
+    answer: "c"
+  },
+  {
+    number: 13,
+    animalImage: "images/cow.png",
+    options: {
+      a: "16",
+      b: "13",
+      c: "15"
+    },
+    answer: "b"
+  },
+  {
+    number: 9,
+    animalImage: "images/chicken.png",
+    options: {
+      a: "6",
+      b: "9",
+      c: "8"
+    },
+    answer: "b"
+  }
+];
+
+/* Matching game data */
+window.matchingGameMediumProblems = [
+
+    "35 - 26",
+    "6 * 0",
+    "40 / 8"
+];
+
+window.matchingGameMediumElements = [
+
+    "nine",
+    "zero",
+    "five"
+];
+
+window.matchingGameMediumProblems2 = [
+
+    "7 * 2",
+    "12 + 5",
+    "20 - 10"
+];
+
+window.matchingGameMediumElements2 = [
+
+    "fourteen",
+    "seventeen",
+    "ten"
+];
+
+window.matchingGameMediumProblems3 = [
+
+    "30 / 10",
+    "19 + 5",
+    "6 * 3"
+];
+
+window.matchingGameMediumElements3 = [
+
+    "three",
+    "twentyFour",
+    "eighteen"
+];
