5f2d391932657bb63eb47332c7e88921cbb7c4bd
paguioc <31397592+paguioc@users.noreply.github.com>
2019-05-27 19:58:25 +1000

    Create easyLevel.js

---
+++ data/easyLevel.js
@@ -0,0 +1,240 @@
+/*
+  This contains the data for the easy level.
+*/
+
+/* Array of numbers to be used in randomising questions */
+window.numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+/* Question and answer game data */
+window.easyLevel = [
+  {
+    question: "2 + 5 = ",
+    answer: 7
+  },
+  {
+    question: "3 + 1 = ",
+    answer: 4
+  },
+  {
+    question: "9 - 1 = ",
+    answer: 8
+  },
+  {
+    question: "2 - 1 = ",
+    answer: 1
+  },
+  {
+    question: "5 * 1 = ",
+    answer: 5
+  },
+  {
+    question: "6 * 0 = ",
+    answer: 0
+  },
+  {
+    question: "4 / 2 = ",
+    answer: 2
+  },
+  {
+    question: "8 / 2 = ",
+    answer: 4
+  },
+  {
+    question: "1 / 1 = ",
+    answer: 1
+  },
+  {
+    question: "6 / 3 = ",
+    answer: 2
+  }
+];
+
+/* Memory game data */
+window.memoryGameElements = [
+  {
+    name: "one",
+    image: "images/one.png"
+  },
+  {
+    name: "two",
+    image: "images/two.png"
+  },
+  {
+    name: "three",
+    image: "images/three.png"
+  },
+  {
+    name: "four",
+    image: "images/four.png"
+  },
+  {
+    name: "five",
+    image: "images/five.png"
+  },
+  {
+    name: "six",
+    image: "images/six.png"
+  },
+  {
+    name: "seven",
+    image: "images/seven.png"
+  },
+  {
+    name: "eight",
+    image: "images/eight.png"
+  },
+  {
+    name: "nine",
+    image: "images/nine.png"
+  }
+
+];
+
+
+/* Multiple Choice game data */
+window.multipleChoiceElements = [
+  {
+    number: 3,
+    animalImage: "images/duck.png",
+    options: {
+      a: "2",
+      b: "3",
+      c: "5"
+    },
+    answer: "b"
+  },
+  {
+    number: 1,
+    animalImage: "images/goat.png",
+    options: {
+      a: "4",
+      b: "3",
+      c: "1"
+    },
+    answer: "c"
+  },
+  {
+    number: 5,
+    animalImage: "images/chicken.png",
+    options: {
+      a: "2",
+      b: "4",
+      c: "5"
+    },
+    answer: "c"
+  },
+  {
+    number: 2,
+    animalImage: "images/sheep.png",
+    options: {
+      a: "2",
+      b: "6",
+      c: "1"
+    },
+    answer: "a"
+  },
+  {
+    number: 6,
+    animalImage: "images/chick.png",
+    options: {
+      a: "6",
+      b: "3",
+      c: "5"
+    },
+    answer: "a"
+  },
+  {
+    number: 4,
+    animalImage: "images/cow.png",
+    options: {
+      a: "7",
+      b: "4",
+      c: "5"
+    },
+    answer: "b"
+  },
+  {
+    number: 7,
+    animalImage: "images/donkey.png",
+    options: {
+      a: "6",
+      b: "7",
+      c: "5"
+    },
+    answer: "b"
+  },
+  {
+    number: 3,
+    animalImage: "images/chicken2.png",
+    options: {
+      a: "4",
+      b: "5",
+      c: "3"
+    },
+    answer: "c"
+  },
+  {
+    number: 5,
+    animalImage: "images/goat2.png",
+    options: {
+      a: "5",
+      b: "3",
+      c: "6"
+    },
+    answer: "a"
+  },
+  {
+    number: 2,
+    animalImage: "images/egg.png",
+    options: {
+      a: "3",
+      b: "2",
+      c: "4"
+    },
+    answer: "b"
+  }
+];
+
+/* Matching game data */
+window.matchingGameProblems = [
+
+    "9 - 6",
+    "5 + 2",
+    "9 - 8"
+];
+
+window.matchingGameElements = [
+
+    "three",
+    "seven",
+    "one"
+];
+
+window.matchingGameProblems2 = [
+
+    "5 + 4",
+    "3 * 2",
+    "8 / 2"
+];
+
+window.matchingGameElements2 = [
+
+    "nine",
+    "six",
+    "four"
+];
+
+window.matchingGameProblems3 = [
+
+    "8 * 1",
+    "7 - 5",
+    "4 + 1"
+];
+
+window.matchingGameElements3 = [
+
+    "eight",
+    "two",
+    "five"
+];
